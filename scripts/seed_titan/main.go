5b2d2d55d57bef6069573183c2105a4ea6526e24
vikash-chandra <vikashchandrajha11@gmail.com>
2026-08-30 00:04:02 +0530

    Fix seed_titan script

--- scripts/seed_titan/main.go
+++ scripts/seed_titan/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	kiteconnect "github.com/zerodha/gokiteconnect/v4"
 	"zerodha-trading/config"
 	"zerodha-trading/data"
 	"zerodha-trading/monitoring"
@@ -34,32 +35,56 @@ func main() {
 		cfg.AccessToken = dbToken
 	}
 
-	brokerClient := data.NewZerodhaBrokerAdapter(cfg.APIKey, cfg.AccessToken, logger.Logger)
-	token := int64(897537) // TITAN
+	rawKiteClient := kiteconnect.New(cfg.APIKey)
+	rawKiteClient.SetAccessToken(cfg.AccessToken)
+	kiteClient := data.NewZerodhaBrokerAdapter(rawKiteClient)
+	token := 897537 // TITAN
 
 	loc, _ := time.LoadLocation("Asia/Kolkata")
 	fromDate := time.Date(2026, 8, 28, 9, 15, 0, 0, loc)
 	toDate := time.Date(2026, 8, 28, 15, 30, 0, 0, loc)
 
 	log.Println("Fetching 1m candles for TITAN...")
-	c1m, err := brokerClient.GetHistoricalCandles(ctx, token, "minute", fromDate, toDate)
+	c1m, err := kiteClient.GetHistoricalData(token, "minute", fromDate, toDate, false, false)
 	if err != nil {
 		log.Printf("Error fetching 1m: %v", err)
 	} else {
 		log.Printf("Fetched %d 1m candles", len(c1m))
 		for _, c := range c1m {
-			_ = db.InsertCandle(ctx, "candles_1m", token, c)
+			color := "DOJI"
+			if c.Close > c.Open {
+				color = "GREEN"
+			} else if c.Close < c.Open {
+				color = "RED"
+			}
+			vwap := (c.Open + c.High + c.Low + c.Close) / 4.0
+			_, _ = db.WithContext(ctx).ExecContext(ctx, `
+				INSERT INTO candles_1m (token, time, open, high, low, close, volume, vwap, bid, ask, tick_count, color)
+				VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+				ON CONFLICT (token, time) DO UPDATE SET open = EXCLUDED.open, high = EXCLUDED.high, low = EXCLUDED.low, close = EXCLUDED.close, volume = EXCLUDED.volume, vwap = EXCLUDED.vwap, color = EXCLUDED.color
+			`, token, c.Date, c.Open, c.High, c.Low, c.Close, int64(c.Volume), vwap, c.Low, c.High, 100, color)
 		}
 	}
 
 	log.Println("Fetching 5m candles for TITAN...")
-	c5m, err := brokerClient.GetHistoricalCandles(ctx, token, "5minute", fromDate, toDate)
+	c5m, err := kiteClient.GetHistoricalData(token, "5minute", fromDate, toDate, false, false)
 	if err != nil {
 		log.Printf("Error fetching 5m: %v", err)
 	} else {
 		log.Printf("Fetched %d 5m candles", len(c5m))
 		for _, c := range c5m {
-			_ = db.InsertCandle(ctx, "candles_5m", token, c)
+			color := "DOJI"
+			if c.Close > c.Open {
+				color = "GREEN"
+			} else if c.Close < c.Open {
+				color = "RED"
+			}
+			vwap := (c.Open + c.High + c.Low + c.Close) / 4.0
+			_, _ = db.WithContext(ctx).ExecContext(ctx, `
+				INSERT INTO candles_5m (token, time, open, high, low, close, volume, vwap, bid, ask, tick_count, color)
+				VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+				ON CONFLICT (token, time) DO UPDATE SET open = EXCLUDED.open, high = EXCLUDED.high, low = EXCLUDED.low, close = EXCLUDED.close, volume = EXCLUDED.volume, vwap = EXCLUDED.vwap, color = EXCLUDED.color
+			`, token, c.Date, c.Open, c.High, c.Low, c.Close, int64(c.Volume), vwap, c.Low, c.High, 500, color)
 		}
 	}
 
